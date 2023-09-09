ea941668e0236dc2b3a073317724bd9e47bd45d6
Riazul Islam <riazul.cse.mbstu@gmail.com>
2023-09-09 15:44:22 +0600

    Update

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -40,7 +40,7 @@ class BankController extends Controller
     //Get bank list by user id
     public function getUserBankListbyUserId(Request $request,$user_id) {
         $userBankList = Bank::orderBy('updated_at', 'desc')
-        ->where('user_id', $user_id)->paginate($limit);
+        ->where('user_id', $user_id)->paginate($request->limit);
         return ResponseUtils::json([
             'code' => Response::HTTP_OK,
             'success' => true,
@@ -77,15 +77,15 @@ class BankController extends Controller
     }
     /// edit bank 
 
-    public function update(Request $request)
+    public function update(Request $request, $id)
     {
 
         $bankId = request("id");
 
         $BankListExist = Bank::where(
             [
-                ['id', $bankId],
-                ['user_id', $request->user->id]
+                ['id', $id],
+                // ['user_id', $request->user->id]
             ]
         )->first();
 
@@ -93,8 +93,8 @@ class BankController extends Controller
             return ResponseUtils::json([
                 'code' => Response::HTTP_BAD_REQUEST,
                 'success' => false,
-                'msg_code' => MsgCode::NO_BANK_EXISTS[0],
-                'msg' => MsgCode::NO_BANK_EXISTS[1],
+                'msg_code' => "NO_BANK",
+                'msg' => "No bank found",
             ]);
         }
 
@@ -113,50 +113,5 @@ class BankController extends Controller
             'data' => $BankListExist
         ]);
     }
-
-
-
-    // public function editBankInfo($bankId, Request $request)
-    // {
-    //     if ($bankId == null || empty($bankId)) {
-    //         return response()->json("Wallet not found");
-    //     }
-
-    //     $editBankInfo = Bank::findOrFail($bankId);
-    //     if ($editBankInfo == null || empty($editBankInfo)) {
-    //         return response()->json('Wallet not found');
-    //     }
-
-    //     $editBankInfo = Bank::where(['id' => $bankId])->first();
-    //     if ($editBankInfo == null) {
-    //         return ResponseUtils::json([
-    //             'code' => Response::HTTP_NOT_FOUND,
-    //             'success' => false,
-    //             'msg_code' => MsgCode::NO_TRANSACTION_EXISTS[0],
-    //             'msg' => MsgCode::NO_TRANSACTION_EXISTS[1],
-    //         ]);
-    //     }
-
-    //     DB::beginTransaction();
-    //     try {
-    //         $response = $editBankInfo->update([
-    //             "bank_code" => $request->bank_code ?? $editBankInfo->bank_code,
-    //             "bank_account_number" => $request->bank_account_number ?? $editBankInfo->bank_account_number,
-    //             "bank_account_holder_name" => $request->bank_account_holder_name ?? $editBankInfo->bank_account_holder_name,
-    //         ]);
-    //         DB::commit();
-    //     } catch (\Exception $e) {
-    //         DB::rollBack();
-    //         throw new Exception($e->getMessage());
-    //     }
-
-    //     return ResponseUtils::json([
-    //         'code' => Response::HTTP_OK,
-    //         'success' => true,
-    //         'msg_code' => MsgCode::SUCCESS[0],
-    //         'msg' => MsgCode::SUCCESS[1],
-    //         'data' => $editBankInfo,
-    //     ]);
-    // }
 
 }
